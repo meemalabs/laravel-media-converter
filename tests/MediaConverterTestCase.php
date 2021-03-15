084a0d531c3d29c96330994ad5a10e5bd2eea1bc
Chris Breuer <chrisbreuer93@gmail.com>
2021-03-15 14:30:52 -0700

    wip

--- tests/MediaConverterTestCase.php
+++ tests/MediaConverterTestCase.php
@@ -2,6 +2,7 @@
 
 namespace Meema\MediaConverter\Tests;
 
+use Illuminate\Support\Facades\Config;
 use Meema\MediaConverter\Providers\MediaConverterServiceProvider;
 use Orchestra\Testbench\TestCase;
 
@@ -25,6 +26,13 @@ class MediaConverterTestCase extends TestCase
     public function initializeSettings()
     {
         $configFile = file_get_contents(__DIR__.'/config/job.json');
+
+        // let's make sure these config values are set
+        Config::set('media-converter.credentials.key', env('AWS_ACCESS_KEY_ID'));
+        Config::set('media-converter.credentials.secret', env('AWS_SECRET_ACCESS_KEY'));
+        Config::set('media-converter.url', env('AWS_MEDIACONVERT_ACCOUNT_URL'));
+        Config::set('media-converter.iam_arn', env('AWS_IAM_ARN'));
+        Config::set('media-converter.queue_arn', env('AWS_QUEUE_ARN'));
 
         $this->settings = json_decode($configFile, true);
     }
