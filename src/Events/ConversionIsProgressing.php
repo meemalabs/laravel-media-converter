04090e733e4b6c29292515d08446fced1b13efe6
Chris <chrisbreuer93@gmail.com>
2026-07-28 20:38:30 -0700

    Apply fixes from StyleCI (#84)

--- src/Events/ConversionIsProgressing.php
+++ src/Events/ConversionIsProgressing.php
@@ -16,7 +16,7 @@ class ConversionIsProgressing
     /**
      * Create a new event instance.
      *
-     * @param $message
+     * @param  $message
      */
     public function __construct($message)
     {
